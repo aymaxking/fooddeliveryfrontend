f3009628dbf75fd96f31d8a4bdae6c0073a69717
BLACKHOLE <aymanehamidat@gmail.com>
2022-03-01 00:16:49 -0800

    2

--- src/app/modules/general-modules/general-modules.component.ts
+++ src/app/modules/general-modules/general-modules.component.ts
@@ -4,6 +4,7 @@ import {ApplicationDeliveryService} from "../../services/AdminServices/applicati
 import {Router} from "@angular/router";
 import {ApplicationPlace} from "../../models/applicationPlace";
 import {ApplicationPlaceService} from "../../services/AdminServices/applicationPlaceService/application-place.service";
+declare  var jQuery:  any;
 
 @Component({
   selector: 'app-general-modules',
@@ -29,6 +30,47 @@ export class GeneralModulesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    (function ($) {
+      $(document).ready(function(){
+        let menu = document.querySelector('#menu-bar');
+        let navbar = document.querySelector('.navbar');
+        // @ts-ignore
+        menu.onclick = () =>{
+          // @ts-ignore
+          menu.classList.toggle('fa-times');
+          // @ts-ignore
+          navbar.classList.toggle('active');
+
+        }
+
+        window.onscroll = () =>{
+
+          // @ts-ignore
+          menu.classList.remove('fa-times');
+          // @ts-ignore
+          navbar.classList.remove('active');
+
+          if(window.scrollY > 60){
+            // @ts-ignore
+            document.querySelector('#scroll-top').classList.add('active');
+          }else{
+            // @ts-ignore
+            document.querySelector('#scroll-top').classList.remove('active');
+          }
+
+        }
+
+        function loader(){
+          // @ts-ignore
+          document.querySelector('.loader-container').classList.add('fade-out');
+        }
+
+        function fadeOut(){
+          setInterval(loader, 3000);
+        }
+        // @ts-ignore
+        window.onload = fadeOut();      });
+    })(jQuery);
   }
   public onImageUpload(event:any) {
     this.uploadedImage = event.target.files[0];
